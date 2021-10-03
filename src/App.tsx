184ebad35a7bb593bbffcaf53cd47ec7b6703bed
FatemaKhalid <fatemakhalid96@gmail.com>
2021-10-03 14:31:02 +0200

    Adding chart to view numbers

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { NavBar } from "./navBar";
+import { Chart } from "./chart";
 
 export function App() {
   return (
     <div className="App">
       <NavBar />
+      <Chart />
     </div>
   );
 }
